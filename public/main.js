ddd6aad6b0a7ad7c75ff69fd946b14f6f9694a4b
sheodox <3468630+sheodox@users.noreply.github.com>
2020-01-29 21:35:04 -0600

    add an alarm sound to be used in the future, and make the UI reflect the state of the tasks.

--- public/main.js
+++ public/main.js
@@ -2,16 +2,41 @@ let workTimeDuration = 5,
 	breakTimeDuration = 1;
 
 function workOnATask() {
-	console.log('Start working!');
-	setTimeout(() => {
-		console.log('Stop working!');
+	setMessage('Start working!');
+	showProgressForDuration(workTimeDuration);
 
-		setTimeout(workOnATask, breakTimeDuration * 1000);
-	}, workTimeDuration * 1000);
+	setTimeout(() => {
+		setMessage('Stop working!');
+
+		showProgressForDuration(breakTimeDuration);
+		setTimeout(workOnATask, breakTimeDuration);
+	}, workTimeDuration);
 }
 
+let updateInterval;
+function showProgressForDuration(duration) {
+	const startTime = Date.now(),
+		bar = query('#status-progress');
+
+	bar.max = duration;
+	clearInterval(updateInterval);
+
+	updateInterval = setInterval(() => {
+		bar.value = Date.now() - startTime;
+	}, 20);
+}
+
+/**
+ * Shortcut for document.querySelector
+ * @param selector - css selector
+ * @returns {HTMLElement}
+ */
 function query(selector) {
 	return document.querySelector(selector);
+}
+
+function setMessage(message) {
+	query('#status').textContent = message;
 }
 
 document
@@ -19,8 +44,8 @@ document
 	.addEventListener('submit', (event) => {
 		event.preventDefault();
 
-		workTimeDuration = parseInt(query('#work-duration').value, 10);
-		breakTimeDuration = parseInt(query('#break-duration').value, 10);
+		workTimeDuration = parseInt(query('#work-duration').value, 10) * 1000;
+		breakTimeDuration = parseInt(query('#break-duration').value, 10) * 1000;
 
 		workOnATask();
 	});
