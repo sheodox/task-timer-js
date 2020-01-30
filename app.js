827fe62fb45297a2dcaf848c1f433fb7dc46dcd7
sheodox <3468630+sheodox@users.noreply.github.com>
2020-01-29 19:38:52 -0600

    add basic static file serving functionality

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const express = require('express'),
+	app = express(),
+	port = 3000;
+
+app.use(express.static('./public'));
+
+app.listen(port, () => console.log(`Todo-Timer app listening on port ${port}!`));
