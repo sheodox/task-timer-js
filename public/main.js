a8ebb182101caf61f485f12706e339d2143fe11b
LINK1733 <zaxios@hotmail.com>
2020-01-30 18:00:10 -0600

    Matched python functionality and added a clear button that clears out the to-do so a user can start over without having to refresh the page. (#1)

--- public/main.js
+++ public/main.js
@@ -1,16 +1,60 @@
 let workTimeDuration = 5,
-	breakTimeDuration = 1;
+	breakTimeDuration = 1,
+	currentToDo = 0,
+ 	toDoArr = [];
+
+query('#to-do-form')
+	.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		const todoInput = query('#to-do');
+
+		if (todoInput.value){
+			toDoArr.push(todoInput.value);
+			updatePreview();
+			todoInput.value = '';
+		}
+	});
+
+query('#clear-button')
+	.addEventListener('click', (event) => {
+		event.preventDefault();
+
+		toDoArr = [];
+		updatePreview();
+	});
+
+function updatePreview() {
+	query('#todo-list-preview').textContent = `Your to-do's: ${toDoArr.join(', ')}`;
+}
 
 function workOnATask() {
+
+	setToDo(toDoArr[currentToDo++]);
 	setMessage('Start working!');
 	showProgressForDuration(workTimeDuration);
 
 	setTimeout(() => {
+		if (currentToDo === toDoArr.length){
+			setToDo("Nothing");
+			setMessage("Congratulations! You have finished your to-do's!");
+			return;
+		}
+		ringAlarm();
 		setMessage('Stop working!');
+		setToDo('Enjoy your break');
 
 		showProgressForDuration(breakTimeDuration);
-		setTimeout(workOnATask, breakTimeDuration);
+		setTimeout(() => {
+			ringAlarm();
+			workOnATask();
+		}, breakTimeDuration);
 	}, workTimeDuration);
+
+}
+
+function ringAlarm() {
+	query('#alert-tone').play();
 }
 
 let updateInterval;
@@ -35,6 +79,10 @@ function query(selector) {
 	return document.querySelector(selector);
 }
 
+function setToDo(message) {
+	query('#currentToDo').textContent = `Current To-Do: ${message}`;
+}
+
 function setMessage(message) {
 	query('#status').textContent = message;
 }
@@ -46,6 +94,6 @@ document
 
 		workTimeDuration = parseInt(query('#work-duration').value, 10) * 1000;
 		breakTimeDuration = parseInt(query('#break-duration').value, 10) * 1000;
-
+		currentToDo = 0;
 		workOnATask();
 	});
