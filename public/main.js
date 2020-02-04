44327230ca2faea593f2873180660627a0915ae4
LINK1733 <zaxios@hotmail.com>
2020-02-03 20:22:15 -0600

    Changed to use promises for workOnATask. (#2)

--- public/main.js
+++ public/main.js
@@ -1,73 +1,80 @@
 let workTimeDuration = 5,
-	breakTimeDuration = 1,
-	currentToDo = 0,
- 	toDoArr = [];
+    breakTimeDuration = 1,
+    currentToDo = 0,
+    toDoArr = [];
 
 query('#to-do-form')
-	.addEventListener('submit', (event) => {
-		event.preventDefault();
+    .addEventListener('submit', (event) => {
+        event.preventDefault();
 
-		const todoInput = query('#to-do');
+        const todoInput = query('#to-do');
 
-		if (todoInput.value){
-			toDoArr.push(todoInput.value);
-			updatePreview();
-			todoInput.value = '';
-		}
-	});
+        if (todoInput.value) {
+            toDoArr.push(todoInput.value);
+            updatePreview();
+            todoInput.value = '';
+        }
+    });
 
 query('#clear-button')
-	.addEventListener('click', (event) => {
-		event.preventDefault();
+    .addEventListener('click', (event) => {
+        event.preventDefault();
 
-		toDoArr = [];
-		updatePreview();
-	});
+        toDoArr = [];
+        updatePreview();
+    });
 
 function updatePreview() {
-	query('#todo-list-preview').textContent = `Your to-do's: ${toDoArr.join(', ')}`;
+    query('#todo-list-preview').textContent = `Your to-do's: ${toDoArr.join(', ')}`;
 }
 
 function workOnATask() {
-
 	setToDo(toDoArr[currentToDo++]);
 	setMessage('Start working!');
 	showProgressForDuration(workTimeDuration);
 
-	setTimeout(() => {
-		if (currentToDo === toDoArr.length){
+    const taskPromise = new Promise(resolve => {
+        setTimeout(resolve, workTimeDuration);
+    });
+
+	if (currentToDo < toDoArr.length){
+		return taskPromise.then(() => {
+			ringAlarm();
+			setMessage('Stop working!');
+			setToDo('Enjoy your break');
+
+			showProgressForDuration(breakTimeDuration);
+			return new Promise(resolve => {
+				setTimeout(resolve, breakTimeDuration);
+			})
+		})
+			.then(workOnATask);
+	}
+	else {
+		return taskPromise.then(() => {
+			ringAlarm();
 			setToDo("Nothing");
 			setMessage("Congratulations! You have finished your to-do's!");
-			return;
-		}
-		ringAlarm();
-		setMessage('Stop working!');
-		setToDo('Enjoy your break');
-
-		showProgressForDuration(breakTimeDuration);
-		setTimeout(() => {
-			ringAlarm();
-			workOnATask();
-		}, breakTimeDuration);
-	}, workTimeDuration);
-
+		});
+	}
 }
 
 function ringAlarm() {
-	query('#alert-tone').play();
+    query('#alert-tone').play();
 }
 
 let updateInterval;
+
 function showProgressForDuration(duration) {
-	const startTime = Date.now(),
-		bar = query('#status-progress');
+    const startTime = Date.now(),
+        bar = query('#status-progress');
 
-	bar.max = duration;
-	clearInterval(updateInterval);
+    bar.max = duration;
+    clearInterval(updateInterval);
 
-	updateInterval = setInterval(() => {
-		bar.value = Date.now() - startTime;
-	}, 20);
+    updateInterval = setInterval(() => {
+        bar.value = Date.now() - startTime;
+    }, 20);
 }
 
 /**
@@ -76,24 +83,24 @@ function showProgressForDuration(duration) {
  * @returns {HTMLElement}
  */
 function query(selector) {
-	return document.querySelector(selector);
+    return document.querySelector(selector);
 }
 
 function setToDo(message) {
-	query('#currentToDo').textContent = `Current To-Do: ${message}`;
+    query('#currentToDo').textContent = `Current To-Do: ${message}`;
 }
 
 function setMessage(message) {
-	query('#status').textContent = message;
+    query('#status').textContent = message;
 }
 
 document
-	.getElementById('times-form')
-	.addEventListener('submit', (event) => {
-		event.preventDefault();
+    .getElementById('times-form')
+    .addEventListener('submit', (event) => {
+        event.preventDefault();
 
-		workTimeDuration = parseInt(query('#work-duration').value, 10) * 1000;
-		breakTimeDuration = parseInt(query('#break-duration').value, 10) * 1000;
-		currentToDo = 0;
-		workOnATask();
-	});
+        workTimeDuration = parseInt(query('#work-duration').value, 10) * 1000;
+        breakTimeDuration = parseInt(query('#break-duration').value, 10) * 1000;
+        currentToDo = 0;
+        workOnATask();
+    });
